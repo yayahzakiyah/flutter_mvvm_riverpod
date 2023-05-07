e7bfb354e9d35f0dbc21ebac424f3c7e2da2de50
yayahzakiyah <yayahzakiyah95@gmail.com>
2023-05-07 21:46:59 +0700

    template MVVM with riverpod

---
+++ lib/data/api/api_helper.dart
@@ -0,0 +1,42 @@
+import 'package:dio/dio.dart';
+
+class ApiHelper {
+  static final Dio _dio = Dio();
+
+  static Future<dynamic> get(String path,
+      {Map<String, dynamic>? queryParameters}) async {
+    try {
+      final response = await _dio.get(path, queryParameters: queryParameters);
+      return response;
+    } on DioError catch (e) {
+      return e;
+    }
+  }
+
+  static Future<dynamic> post(String path, Map<String, dynamic> data) async {
+    try {
+      final response = await _dio.post(path, data: data);
+      return response;
+    } on DioError catch (e) {
+      return e;
+    }
+  }
+
+  static Future<dynamic> put(String path, Map<String, dynamic> data) async {
+    try {
+      final response = await _dio.put(path, data: data);
+      return response;
+    } on DioError catch (e) {
+      return e;
+    }
+  }
+
+  static Future<dynamic> delete(String path, Map<String, dynamic> data) async {
+    try {
+      final response = await _dio.delete(path, data: data);
+      return response;
+    } on DioError catch (e) {
+      return e;
+    }
+  } 
+}
